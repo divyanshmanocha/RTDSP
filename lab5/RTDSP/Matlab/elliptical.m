9b4767c534111c4a261bc619316cbfd9c199517e
Divyansh Manocha <dan12n@users.noreply.github.com>
2018-02-26 17:11:52 +0000

    Fixed

--- lab5/RTDSP/Matlab/elliptical.m
+++ lab5/RTDSP/Matlab/elliptical.m
@@ -1,8 +1,8 @@
 function [b,a] = elliptical (Fs)
 
 order = 4; % Order of the bandpass filter
-startband = 270/Fs; % Normalised startband frequency
-stopband = 450/Fs; % Normalised stopband frequency
+startband = 270*2/Fs; % Normalised startband frequency
+stopband = 450*2/Fs; % Normalised stopband frequency
 ripple = 0.3; % The passband ripple (in dB)
 stopband_atten = 20; % The stopband attenuation (in dB)
 
