2a7a81942e8603689517eaf119b3e847c6cf5f0b
ymherklotz <ymherklotz@gmail.com>
2018-02-27 10:34:49 +0000

    Fixed proj folder for lab5ex2

--- lab5/RTDSP/Matlab/gen_filter.m
+++ lab5/RTDSP/Matlab/gen_filter.m
@@ -1,5 +1,6 @@
 Fs = 8000;
-b = m_filter;
-freqz(b, 1, 1024, Fs);
+[b,a] = elliptical (Fs);
+freqz(b, a);
 
-save filter_coeff_correct.txt b -ASCII -DOUBLE -TABS
+%save filter_coeff_ell_b.txt b -ASCII -DOUBLE -TABS
+%save filter_coeff_ell_a.txt a -ASCII -DOUBLE -TABS
